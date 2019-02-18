6ee320427986ee3a53c2202e3b682a76aec02acc
Tony's Macbook <cstony0917@gmail.com>
2019-02-18 20:19:36 +0800

    Updates

---
+++ precache-manifest.d62da4e16d2814cb66cca5277d84b9ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3d4c061c0eab9a7828312aa9b882852",
+    "url": "/static/media/Sad-Violin.d3d4c061.mp3"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9fa6b33f44c413d722f9",
+    "url": "/static/js/main.9fa6b33f.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "9fa6b33f44c413d722f9",
+    "url": "/static/css/main.b4c38240.chunk.css"
+  },
+  {
+    "revision": "5c620d97ee07ac0ce9e33606aefc6194",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aeeec8c2681719a8b221de547d8aa4cc.js"
+  "/precache-manifest.d62da4e16d2814cb66cca5277d84b9ed.js"
 );
 
 workbox.clientsClaim();
