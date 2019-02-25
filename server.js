be92c1371ad164d32c64543f9d8e2b07d0d71fd9
Tony's Macbook <cstony0917@gmail.com>
2019-02-26 04:14:22 +0800

    replace old start script

--- server.js
+++ server.js
@@ -3,30 +3,27 @@ require('dotenv').config()
 // Constants
 const express = require('express')
 const env = process.env.NODE_ENV || 'development'
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 const HOST = '0.0.0.0'
 
 // App
 const app = express()
 
-
-
 if (env === 'development') {
-  // const webpackConfig = require('../webpack.config')
-  const compiler = require('webpack')({})
+  const webpackConfig = require('./config/webpack.config')(env)
+  const compiler = require('webpack')(webpackConfig)
 
   app.use(require('webpack-dev-middleware')(compiler, {
-    publicPath: '/assets/',
+    publicPath: '/',
   }))
-
-  // app.use(require('webpack-hot-middleware')(compiler, {
-  //   log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
-  // }))
+  app.use(require('webpack-hot-middleware')(compiler, {
+    log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
+  }))
   // app.use(require('webpack-hot-middleware')(compiler))
+} else {
+  app.use(express.static('./build'))
 }
 
-
-app.use(express.static('./build'))
 
 // app.get('/', (req, res) => {
 //   res.send('Hello world\n')
