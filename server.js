962709230b9dbddd82210214408ac5a34be0c862
Tony's Macbook <cstony0917@gmail.com>
2019-02-26 01:42:29 +0800

    remove service worker

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
-
-
-const express = require('express')
+require('dotenv').config()
 
 // Constants
+const express = require('express')
+const env = process.env.NODE_ENV || 'development'
 const PORT = 8080
 const HOST = '0.0.0.0'
 
 // App
 const app = express()
+
+
+
+if (env === 'development') {
+  // const webpackConfig = require('../webpack.config')
+  const compiler = require('webpack')({})
+
+  app.use(require('webpack-dev-middleware')(compiler, {
+    publicPath: '/assets/',
+  }))
+
+  // app.use(require('webpack-hot-middleware')(compiler, {
+  //   log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
+  // }))
+  // app.use(require('webpack-hot-middleware')(compiler))
+}
+
 
 app.use(express.static('./build'))
 
